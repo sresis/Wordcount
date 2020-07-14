2f7883538fda8f637e892610856c39441a65ad93
Stephanie Resis <sresis@umich.edu>
2020-07-14 00:46:40 +0000

    added function descriptions

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,7 @@
 import sys
 file = open(sys.argv[1])
 
+# returns all words from file and gets rid of punctuation/capitals
 def get_words(file):
 	all_lines = []
 	for line in file:
@@ -10,7 +11,7 @@ def get_words(file):
 			all_lines.append(word)
 
 	return all_lines
-
+# returns count of each word
 def get_count(words):
 	word_count = {}
 	for item in words:
@@ -19,9 +20,8 @@ def get_count(words):
 		word_count[item] = word_count.get(item, 0) + 1
 	return word_count
 
-
+# returns sorted dictionary
 def sort_dict(dictionary):
-
 	sorted_dict = sorted((value, key) for (key, value) in dictionary.items())
 	sorted_dict.reverse()
 	new_list = {}
@@ -29,6 +29,7 @@ def sort_dict(dictionary):
 		new_list[value] = index
 	return new_list
 
+# main function. Prints word: word count for each word in the text
 def main():
 	words = get_words(file)
 	word_count = get_count(words)
